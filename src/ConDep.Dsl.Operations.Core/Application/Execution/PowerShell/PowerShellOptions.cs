a03f87c440408b4ddd2b5280a0016acd13a9cb16
Jon Arild Torresdal <jon@torresdal.net>
2014-07-17 21:51:39 +0200

    Extracted out all operations into seperate assembly

---
+++ src/ConDep.Dsl.Operations.Core/Application/Execution/PowerShell/PowerShellOptions.cs
@@ -0,0 +1,29 @@
+﻿namespace ConDep.Dsl.Operations.Application.Execution.PowerShell
+{
+    public class PowerShellOptions : IOfferPowerShellOptions
+    {
+        private readonly PowerShellOptionValues _values = new PowerShellOptionValues();
+
+        public IOfferPowerShellOptions RequireRemoteLib()
+        {
+            _values.RequireRemoteLib = true;
+            return this;
+        }
+
+        public IOfferPowerShellOptions ContinueOnError(bool value)
+        {
+            _values.ContinueOnError = value;
+            return this;
+        }
+
+        public PowerShellOptionValues Values { get { return _values; } }
+
+        public class PowerShellOptionValues
+        {
+            public bool RequireRemoteLib { get; set; }
+
+            public bool ContinueOnError { get; set; }
+        }
+    }
+
+}
