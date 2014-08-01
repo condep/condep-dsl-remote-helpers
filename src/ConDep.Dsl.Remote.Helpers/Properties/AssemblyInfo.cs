d3b110419de579cd28c26b241953c2ad7f06955d
Jon Arild Torresdal <jon@torresdal.net>
2014-08-01 12:53:19 +0200

    Version bump

--- src/ConDep.Dsl.Remote.Helpers/Properties/AssemblyInfo.cs
+++ src/ConDep.Dsl.Remote.Helpers/Properties/AssemblyInfo.cs
@@ -11,8 +11,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCompanyAttribute("ConDep")]
 [assembly: AssemblyProductAttribute("")]
 [assembly: AssemblyCopyrightAttribute("Copyright © ConDep 2014")]
-[assembly: AssemblyVersionAttribute("3.0.2.0")]
-[assembly: AssemblyFileVersionAttribute("3.0.2.0")]
+[assembly: AssemblyVersionAttribute("3.0.3.0")]
+[assembly: AssemblyFileVersionAttribute("3.0.3.0")]
 [assembly: CLSCompliant(true)]
 
 [assembly: Guid("9ca7b9c9-5690-462a-8923-8b31b9e4fa87")]
