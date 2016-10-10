82462001bbc5f1ed4b8c86517c2cdf04698f86f3
Kjell Iver Breistøl <104171kjbr@Z63>
2016-10-10 20:13:53 +0200

    Added build and deploy pipeline for beta packages to myget. changed taget framewørk to .NET 4.5.2

--- src/ConDep.Dsl.Remote.Helpers/Properties/AssemblyInfo.cs
+++ src/ConDep.Dsl.Remote.Helpers/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProductAttribute("")]
 [assembly: AssemblyCopyrightAttribute("Copyright © ConDep 2014")]
 [assembly: AssemblyVersionAttribute("3.1.0.0")]
-[assembly: AssemblyFileVersionAttribute("3.1.0.0")]
+[assembly: AssemblyFileVersionAttribute("3.1.0-beta")]
 [assembly: CLSCompliant(true)]
 
 [assembly: Guid("9ca7b9c9-5690-462a-8923-8b31b9e4fa87")]
